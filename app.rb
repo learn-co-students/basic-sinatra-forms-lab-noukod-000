c87cb425a2c9f7b11057f2b3485b25f92097d5a3
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-05 21:17:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,26 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+      erb :newteam
+    end
+
+    post '/team' do
+  #original_string = params["string"]
+  #@reversed_string = original_string.reverse
+  #puts @reversed_string
+  #erb :reversed
+
+  @name = params[:name]
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @sg= params[:sg]
+  @pf = params[:pf]
+  @sf = params[:sf]
+  @c = params[:c]
+
+  erb :team
+
+   end
 
 end
